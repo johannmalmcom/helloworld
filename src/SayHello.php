9d1032da9294dc5a45128bbb23a55b4c21c182ee
Johann Malm <jmcom@Johanns-MacBook.lan>
2020-03-10 17:01:00 +0100

    First commit

---
+++ src/SayHello.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace HelloWorld;
+
+class SayHello {
+    
+    public static function talk() {
+        return "Hello, world!";
+    }
+    
+}
+
+?>
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once './vendor/autoload.php'; // Autoload files using Composer autoload
+
+use HelloWorld\SayHello;
+
+echo SayHello::talk();
+
+?>
